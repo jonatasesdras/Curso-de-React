635680f38f9b35d19fe227df0332d3cd42df98de
Jonatas Esdras <filatasjon@gmail.com>
2020-07-19 13:38:36 -0300

    Criação do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: ''
+  }
+
+  constructor() {
+    super()
+    this.modificarNome = this.modificarNome.bind(this)
+  }
+
+  modificarNome(event) {
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = ["Jonatas", "Esdras"]
+    const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  render() {
+    const MeuComboBox = () => this.criaComboBox()
+    
+    return (
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Olá {this.state.nome}</h1>
+        <MeuComboBox />
+      </>
+    )
+  }
 }
 
 export default App;
